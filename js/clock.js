daa1491968ea7c83bd07f14c1d8c828ab1921c9e
cjavier70 <rocker5893@yahoo.com>
2015-02-19 21:27:24 -0800

    fix large issue please

--- js/clock.js
+++ js/clock.js
@@ -191,16 +191,21 @@ var addAlarm = function() {
     var alarmObject = new AlarmObject();
 	  //IF LOGGED IN IT SET
 	  if(loggedIn) {
-		  alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": alarmName, fbId: '1552158565069405'}, {
+		  alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": alarmName, fbId: '1552158565069405'}. {
+			  success: function(object) {
+				insertAlarm(hours, mins, ampm, alarmName, object.id);
+				hideAlarmPopup();
+			  }
+		});
 	  }
 	  else {
 		  alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": alarmName, fbId: null}, {
+			  success: function(object) {
+				insertAlarm(hours, mins, ampm, alarmName, object.id);
+				hideAlarmPopup();
+			  }
+		});
 	  }
-      success: function(object) {
-	  	insertAlarm(hours, mins, ampm, alarmName, object.id);
-		hideAlarmPopup();
-      }
-    });
 }
 
 var getAllAlarms = function(userId) {
@@ -219,6 +224,7 @@ var getAllAlarms = function(userId) {
 					fbId = results[i].get("fbId");
 				//useless line
 				if(userId != null)
+					//DONT FUCK WITH INSERTALARM
 	                insertAlarm(hr, min, ampm, name, results[i].id);
             }
         }
